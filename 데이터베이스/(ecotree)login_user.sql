fa7d3bb4928618eaffa6c16c240a79f908685e82
Donghyun0221 <89954934+Donghyun0221@users.noreply.github.com>
2023-12-27 12:45:40 +0900

    Add files via upload

---
+++ 데이터베이스/(ecotree)login_user.sql
@@ -0,0 +1,10 @@
+--------------------------------------------------------
+--  파일이 생성됨 - 금요일-12월-22-2023   
+--------------------------------------------------------
+REM INSERTING into ECO.LOGIN_USER
+SET DEFINE OFF;
+Insert into ECO.LOGIN_USER (USER_ID,USER_PW,USER_NM,USER_EMAIL,USER_CREATE,USER_ADD_PLAT,USER_ADD_NEWPLAT,VERIFY,APP_YN) values ('1234','$2a$10$0.pa9OruF14aCveei90bmO8/L0TFeLXKBZXJQq/cuSUGgxvcdH8Vu','1234','1234',to_date('23/12/04','RR/MM/DD'),'1234','1234',0,'N');
+Insert into ECO.LOGIN_USER (USER_ID,USER_PW,USER_NM,USER_EMAIL,USER_CREATE,USER_ADD_PLAT,USER_ADD_NEWPLAT,VERIFY,APP_YN) values ('admin','$2a$10$r4jzTHDhUYIywXgsQE4RFOyOGRtLknST.e8FMDm3M91OcGhwND6Ra','관리자','admin@gmail.com',to_date('23/12/04','RR/MM/DD'),'admin','admin',9,'Y');
+Insert into ECO.LOGIN_USER (USER_ID,USER_PW,USER_NM,USER_EMAIL,USER_CREATE,USER_ADD_PLAT,USER_ADD_NEWPLAT,VERIFY,APP_YN) values ('qwer','$2a$10$Tyg6WhNQqmyJeO81hhMAtuY692K8N58zvgwPDidW/TkukPJdI5KJ2','qwer','qwer@naver.com',to_date('23/12/04','RR/MM/DD'),'대전광역시 대덕구 오정동 100-12번지','qwer',0,'Y');
+Insert into ECO.LOGIN_USER (USER_ID,USER_PW,USER_NM,USER_EMAIL,USER_CREATE,USER_ADD_PLAT,USER_ADD_NEWPLAT,VERIFY,APP_YN) values ('abcd','$2a$10$x5QkpqTFXRhx8hhNDn8Kb.sALypTpCDQpeR/7e9ftmo1CHXtMM5yW','사원기','41gi@naver.com',to_date('23/12/06','RR/MM/DD'),'대전광역시 대덕구 오정동 100-15번지','대전광역시 대덕구 한밭대로1006번길 74',0,'Y');
+Insert into ECO.LOGIN_USER (USER_ID,USER_PW,USER_NM,USER_EMAIL,USER_CREATE,USER_ADD_PLAT,USER_ADD_NEWPLAT,VERIFY,APP_YN) values ('leeapgil','$2a$10$YA.bHUSJjNakmP27gfk9MuDjeENryWfimer/jykGkJBZbmF1UPKn2','김동현','123123@123123',to_date('23/12/21','RR/MM/DD'),'대전광역시 중구 태평동381-17번지','대전광역시 중구 태평동 381-17번지',0,'Y');
